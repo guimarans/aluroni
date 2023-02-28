ab4623a28a466edc87e6389fb80459f3017d81cc
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-02-28 17:57:02 -0300

    feat: populando cada item

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,24 +1,35 @@
 import styles from './Item.module.scss';
-import logo from 'assets/logo.svg';
-import { PerformanceObserver } from 'perf_hooks';
+import cardapio from '../itens.json';
 
+type Props = typeof cardapio[0];
 
-export default function Item() {
+export default function Item(props: Props) {
+    const {
+        title,
+        description,
+        photo,
+        size,
+        serving,
+        price,
+        id,
+        category
+    } = props;
+
     return (
         <div className={styles.item}>
             <div className={styles.item__imagem}>
-                <img src={logo} alt="imagem" />
+                <img src={photo} alt={title} />
             </div>
             <div className={styles.item__descricao}>
                 <div className={styles.item__titulo}>
-                    <h2></h2>
-                    <p></p>
+                    <h2> {title} </h2>
+                    <p> {description} </p>
                 </div>
                 <div className={styles.item__tags}>
-                    <div className={styles.item__tipo}> </div>
-                    <div className={styles.item__porcao}></div>
-                    <div className={styles.item__qtdpessoas}></div>
-                    <div className={styles.item__valor}></div>
+                    <div className={styles.item__tipo}> {category.label} </div>
+                    <div className={styles.item__porcao}> {size}g </div>
+                    <div className={styles.item__qtdpessoas}> Serve {serving} pessoa{serving === 1 ? '' : 's'}</div>
+                    <div className={styles.item__valor}> R$ {price.toFixed(2)} </div>
                 </div>
             </div>
         </div>
