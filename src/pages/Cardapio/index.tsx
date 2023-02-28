26a552f26dac2a60cb9120eec18c3a0f4ea3f013
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-02-28 16:30:03 -0300

    feat: criando o ordenador

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,12 +1,14 @@
+import styles from './Cardapio.module.scss';
 import { ReactComponent as Logo } from 'assets/logo.svg';
 import { useState } from 'react';
 import Buscador from './Buscador';
-import styles from './Cardapio.module.scss'
 import Filtros from './Filtros';
+import Ordenador from './Ordenador';
 
 export default function Cardapio() {
     const [busca, setBusca] = useState('');
     const [filtro, setFiltro] = useState<number | null>(null);
+    // const [ordenador, setOrdenador] = useState('');
 
     return(
         <main>
@@ -21,8 +23,10 @@ export default function Cardapio() {
             <section className={styles.cardapio}>
                 <h3 className={styles.cardapio__titulo}>Cardápio</h3>
                 <Buscador busca={busca} setBusca={setBusca} />
+
                 <div className={styles.cardapio__filtros}>
                     <Filtros filtro={filtro} setFiltro={setFiltro} />
+                    <Ordenador />
                 </div>
             </section>
         </main>
