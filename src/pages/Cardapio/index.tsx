781629c821cd603f9f7e4ca1be85096c6cb96ab7
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-03-07 23:23:36 -0300

    feat: inclusao de header padrao e ajuste das rotas com outlet

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -11,22 +11,15 @@ export default function Cardapio() {
   const [ordenador, setOrdenador] = useState('');
 
   return(
-    <main>
-      <header className={styles.header}>
-        <div className={styles.header__text}>
-                    A casa do código e da massa
-        </div>
-      </header>
-      <section className={styles.cardapio}>
-        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
-        <Buscador busca={busca} setBusca={setBusca} />
+    <section className={styles.cardapio}>
+      <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+      <Buscador busca={busca} setBusca={setBusca} />
 
-        <div className={styles.cardapio__filtros}>
-          <Filtros filtro={filtro} setFiltro={setFiltro} />
-          <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
-        </div>
-        <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
-      </section>
-    </main>
+      <div className={styles.cardapio__filtros}>
+        <Filtros filtro={filtro} setFiltro={setFiltro} />
+        <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
+      </div>
+      <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
+    </section>
   );
 }
