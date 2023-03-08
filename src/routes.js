8db3a2d8df9424295f00fec776c9c0ff55bee179
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-03-08 14:39:42 -0300

    feat: desenvolvimento da pagina 404

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Sobre from 'pages/Sobre';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import NotFound from 'pages/NotFound';
 
 export default function AppRouter() {
   return (
@@ -17,6 +18,7 @@ export default function AppRouter() {
             <Route path='cardapio' element={<Cardapio />} />
             <Route path='sobre' element={<Sobre />} />
           </Route>
+          <Route path='*' element={<NotFound />} />
         </Routes>
         <Footer />
       </Router>
