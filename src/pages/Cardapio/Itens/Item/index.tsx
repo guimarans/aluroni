366f93e1ace2665213ffa36e6d01d9196587795f
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-03-07 22:06:23 -0300

    feat: inclusao de css da pagina inicio

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,5 +1,5 @@
 import styles from './Item.module.scss';
-import cardapio from '../itens.json';
+import cardapio from 'data/Cardapio.json';
 import classNames from 'classnames';
 
 type Props = typeof cardapio[0];
--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,10 +1,29 @@
-import Cardapio from 'pages/Cardapio/Itens/itens.json';
+import cardapio from 'data/Cardapio.json';
+import styles from './Inicio.module.scss';
 
 export default function Inicio() {
+  let pratosRecomendados = [...cardapio];
+  pratosRecomendados = pratosRecomendados.sort(() => 0.5 - Math.random()).splice(0,3);
+
   return(
     <section>
-      <h3>Recomendações da cozinha</h3>
-      <div></div>
+      <h3 className={styles.titulo}>Recomendações da cozinha</h3>
+      <div className={styles.recomendados}>
+        {pratosRecomendados.map(item => 
+          (<div key={item.id} className={styles.recomendado}>
+            <picture className={styles.recomendado__imagem}>
+              <img src={item.photo} alt={item.title} />
+            </picture>
+            <button className={styles.recomendado__botao}> Ver mais </button>
+          </div>)
+        )};
+      </div>
+
+      <div className={styles.nossaCasa}>
+        <img src="" alt="" />
+
+        <address className={styles.nossaCassa__endereco}></address>
+      </div>
     </section>
   );
 }
