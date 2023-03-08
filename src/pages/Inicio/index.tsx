2395e7c2af33a1db2776d6c4f9abe41dbada1bf8
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-03-07 21:33:22 -0300

    feat: inclusao de arquivo inicio

---
+++ src/pages/Inicio/index.tsx
@@ -0,0 +1,10 @@
+import Cardapio from 'pages/Cardapio/Itens/itens.json';
+
+export default function Inicio() {
+  return(
+    <section>
+      <h3>Recomendações da cozinha</h3>
+      <div></div>
+    </section>
+  );
+}
