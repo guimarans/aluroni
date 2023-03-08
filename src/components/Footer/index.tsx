bb3beb7d87e488880b54055c080316b2c8af95d1
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-03-08 00:36:34 -0300

    feat: inicio da tela not found

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ import { ReactComponent as Logo } from 'assets/logo.svg';
 import styles from './Footer.module.scss';
 
 export default function footer() {
-  return(
+  return (
     <footer className={styles.footer}>
       <Logo />
     </footer>
