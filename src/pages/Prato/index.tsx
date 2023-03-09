2776fba66f55b7b6066f27cd6e03a480a57fbae6
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-03-08 23:41:05 -0300

    feat: criando a rota de prato

---
+++ src/pages/Prato/index.tsx
@@ -0,0 +1,9 @@
+import styles from './Prato.module.scss'; 
+
+export default function Prato() {
+  return (
+    <div>
+      Prato
+    </div>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
 import NotFound from 'pages/NotFound';
 import Sobre from 'pages/Sobre';
+import Prato from 'pages/Prato';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function AppRouter() {
@@ -18,6 +19,7 @@ export default function AppRouter() {
             <Route path='cardapio' element={<Cardapio />} />
             <Route path='sobre' element={<Sobre />} />
           </Route>
+          <Route path= 'prato/:id' element={<Prato />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
         <Footer />
