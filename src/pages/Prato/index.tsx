590cf34486d9af48f990c12e9fab57837921b4ce
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-03-14 17:04:58 -0300

    refactor: componente de tags e ajuste da pagina de pratos para incluir header

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -1,7 +1,7 @@
 import styles from './Prato.module.scss';
 import { useParams, useNavigate } from 'react-router-dom';
-import classNames from 'classnames';
 import cardapio from 'data/cardapio.json';
+import TagsPrato from 'components/TagsPrato';
 
 export default function Prato() {
   const navigate = useNavigate();
@@ -25,24 +25,7 @@ export default function Prato() {
         </picture>
         <div className={styles.conteudo}>
           <p className={styles.conteudo__descricao}> {prato.description} </p>
-          <div className={styles.tags}>
-            <span className={classNames({
-              [styles.tags__tipo]: true,
-              [styles[`tags__tipo__${prato.category.label.toLowerCase()}`]]: true
-            })}>
-              {prato.category.label}
-            </span>
-            <span className={styles.tags__porcao}>
-              {prato.size}g
-            </span>
-            <span className={styles.tags__qtdpessoas}>
-              Serve {prato.serving} pessoa{prato.serving === 1 ? '' : 's'}
-            </span>
-            <span className={styles.tags__valor}>
-              {prato.price.toFixed(2)}
-            </span>
-
-          </div>
+          <TagsPrato {...prato} />
         </div>
       </section>
     </>
