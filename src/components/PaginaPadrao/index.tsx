6a7b48de95977c231949e948ac5c1d81bc5ad23f
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-03-14 17:42:16 -0300

    feat: resolvendo o header com routes e route

--- src/components/PaginaPadrao/index.tsx
+++ src/components/PaginaPadrao/index.tsx
@@ -1,6 +1,6 @@
 import styles from './PaginaPadrao.module.scss';
-import { Outlet } from 'react-router-dom';
 import stylesTema from 'styles/Tema.module.scss';
+import { Outlet } from 'react-router-dom';
 
 export default function PaginaPadrao() {
   return (
